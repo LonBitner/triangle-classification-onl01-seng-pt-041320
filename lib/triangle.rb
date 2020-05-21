7bbb4f35e77a97d5cc3f7491e8a96c54e57d871d
Lon Bitner <lonbitner@gmail.com>
2020-05-21 21:09:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,8 @@ class Triangle
 
   def kind()
     if (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_3 + @side_1) <= @side_2
+       
+       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
